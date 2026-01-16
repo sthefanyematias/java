81aa7c3689d4b8af9588f33212e940ce1e97690c
Sthefany Evangelista Matias <117083316+sthefanyematias@users.noreply.github.com>
2026-01-16 11:33:14 -0300

    Create Livro.java

---
+++ poo/colecoes/Livro.java
@@ -0,0 +1,13 @@
+public class Livro {
+    String titulo;
+    String autor;
+
+    Livro(String titulo, String autor) {
+        this.titulo = titulo;
+        this.autor = autor;
+    }
+
+    String getTitulo() {
+        return titulo;
+    }
+}
