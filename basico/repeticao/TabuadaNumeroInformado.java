0e7398bb6161968587b32d402d1074a609fd5283
Sthefany Evangelista Matias <117083316+sthefanyematias@users.noreply.github.com>
2026-01-16 09:29:34 -0300

    Create TabuadaNumeroInformado.java

---
+++ basico/repeticao/TabuadaNumeroInformado.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class TabuadaNumeroInformado {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numero = scanner.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(numero * i);
+        }
+
+        scanner.close();
+    }
+}
