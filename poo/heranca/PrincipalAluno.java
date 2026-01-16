d5e6be0bcb61a2e32c9378bbd113dc1b4972e0c1
Sthefany Evangelista Matias <117083316+sthefanyematias@users.noreply.github.com>
2026-01-16 11:18:56 -0300

    Create PrincipalAluno.java

---
+++ poo/heranca/PrincipalAluno.java
@@ -0,0 +1,8 @@
+public class PrincipalAluno {
+    public static void main(String[] args) {
+        Aluno a = new Aluno("Sthefany", 20, "ADS");
+
+        a.mostrarDados();
+        a.mostrarCurso();
+    }
+}
