42851e12759039098f189946cff1e1945b62fdb1
Sthefany Evangelista Matias <117083316+sthefanyematias@users.noreply.github.com>
2026-01-16 11:03:03 -0300

    Create PrincipalProduto.java

---
+++ poo/fundamentos/PrincipalProduto.java
@@ -0,0 +1,9 @@
+public class PrincipalProduto {
+    public static void main(String[] args) {
+        Produto p = new Produto();
+        p.nome = "Caderno";
+        p.preco = 25.50;
+
+        p.mostrarProduto();
+    }
+}
