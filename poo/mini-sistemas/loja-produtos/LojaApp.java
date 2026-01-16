f877ad57dd014692c874d9f8022c58e4b317455c
Sthefany Evangelista Matias <117083316+sthefanyematias@users.noreply.github.com>
2026-01-16 12:25:00 -0300

    Create LojaApp.java

---
+++ poo/mini-sistemas/loja-produtos/LojaApp.java
@@ -0,0 +1,9 @@
+public class LojaApp {
+    public static void main(String[] args) {
+        Produto p1 = new ProdutoFisico(100, 20);
+        Produto p2 = new ProdutoDigital(80);
+
+        System.out.println(p1.calcularPrecoFinal());
+        System.out.println(p2.calcularPrecoFinal());
+    }
+}
