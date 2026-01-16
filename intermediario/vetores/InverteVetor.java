12b31c3685ec346f77f3937c446d2ba8c8eb892f
Sthefany Evangelista Matias <117083316+sthefanyematias@users.noreply.github.com>
2026-01-16 09:51:35 -0300

    Create InverteVetor.java

---
+++ intermediario/vetores/InverteVetor.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class InverteVetor {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numeros = new int[5];
+
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = scanner.nextInt();
+        }
+
+        for (int i = numeros.length - 1; i >= 0; i--) {
+            System.out.println(numeros[i]);
+        }
+
+        scanner.close();
+    }
+}
