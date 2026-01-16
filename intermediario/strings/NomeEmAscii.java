1a4a236f309679770f63928628021ed5905fc333
Sthefany Evangelista Matias <117083316+sthefanyematias@users.noreply.github.com>
2026-01-16 10:18:56 -0300

    Create NomeEmAscii.java

---
+++ intermediario/strings/NomeEmAscii.java
@@ -0,0 +1,9 @@
+public class NomeEmAscii {
+    public static void main(String[] args) {
+        char[] nome = {'S', 't', 'h', 'e', 'f', 'a', 'n', 'y'};
+
+        for (int i = 0; i < nome.length; i++) {
+            System.out.println((int) nome[i]);
+        }
+    }
+}
