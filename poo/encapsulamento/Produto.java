5f82119f4128a242ac89deae33a24e565ab7478d
Sthefany Evangelista Matias <117083316+sthefanyematias@users.noreply.github.com>
2026-01-16 11:16:45 -0300

    Create Produto.java

---
+++ poo/encapsulamento/Produto.java
@@ -0,0 +1,23 @@
+public class Produto {
+    private String nome;
+    private double preco;
+
+    public Produto(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        if (preco > 0) {
+            this.preco = preco;
+        }
+    }
+}
