b27190d6804436cad96c121d1a4656ffb27d596a
Sthefany Evangelista Matias <117083316+sthefanyematias@users.noreply.github.com>
2026-01-16 13:07:50 -0300

    Create Produto.java

---
+++ projetos/loja/Produto.java
@@ -0,0 +1,11 @@
+public abstract class Produto {
+    String nome;
+    double preco;
+
+    public Produto(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public abstract void info();
+}
