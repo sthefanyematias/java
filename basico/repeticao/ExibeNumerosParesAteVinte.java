cd1c90ffc0e2abb0632c80c780bad6dbdc653b73
Sthefany Evangelista Matias <117083316+sthefanyematias@users.noreply.github.com>
2026-01-16 09:29:08 -0300

    Create ExibeNumerosParesAteVinte.java

---
+++ basico/repeticao/ExibeNumerosParesAteVinte.java
@@ -0,0 +1,7 @@
+public class ExibeNumerosParesAteVinte {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 20; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
