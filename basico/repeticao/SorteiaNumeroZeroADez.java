79687307e8280ffbe9417d62fe67c9643ae93459
Sthefany Evangelista Matias <117083316+sthefanyematias@users.noreply.github.com>
2026-01-16 09:35:18 -0300

    Create SorteiaNumeroZeroADez.java

---
+++ basico/repeticao/SorteiaNumeroZeroADez.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+
+public class SorteiaNumeroZeroADez {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int numero = random.nextInt(11);
+        System.out.println(numero);
+    }
+}
